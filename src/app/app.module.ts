be46a8118d4c3781e08c466ba68ddcaa4184b546
Christopher K <the4thplayer@hotmail.com>
2020-07-09 22:18:31 +0300

    adding app skeleton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// Components
+import { HeaderComponent } from './app-header/app-header.component';
+import { FooterComponent } from './app-footer/app-footer.component';
+import { AppBlogComponent } from './app-blog/app-blog.component';
+import { AppCalendarComponent } from './app-calendar/app-calendar.component';
+import { AppMainComponent } from './app-main/app-main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AppBlogComponent,
+    AppCalendarComponent,
+    AppMainComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
