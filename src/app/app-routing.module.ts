1a198aba1ce110cfa736e5ad617814e937bc5e50
Christopher K <the4thplayer@hotmail.com>
2020-07-10 00:08:51 +0300

    adding routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppMainComponent } from './app-main/app-main.component';
+import { AppCalendarComponent } from './app-calendar/app-calendar.component';
+import { AppBlogComponent } from './app-blog/app-blog.component';
 
 
-const routes: Routes = [];
+
+
+const appRoutes: Routes = [
+  {
+      path: 'home',
+      component: AppMainComponent
+    },
+    {
+      path: 'activities',
+      component: AppCalendarComponent
+    },
+    {
+      path: 'blog',
+      component: AppBlogComponent
+    }
+]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
